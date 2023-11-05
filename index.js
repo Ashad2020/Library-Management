830a22f61c3032a1a7332078db72d2ce59eb3ee0
Md Ashad <mdashadnstu97@gmail.com>
2023-11-05 18:26:57 +0600

    jwt access token created and gateman middleware created

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +11,7 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.abv0rui.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,7 +33,23 @@ async function run() {
     const CategoriesCollection = database.collection("Categories");
     const BooksCollection = database.collection("AllBooks");
 
-    app.get("/api/v1/categories", async (req, res) => {
+    const gateman = (req, res, next) => {
+      const { token } = req.cookies;
+      console.log(token);
+      if (!token) {
+        return res.status(401).send("You are unauthorized");
+      }
+
+      jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+        if (err) {
+          return res.status(401).send("You are unauthorized");
+        }
+        console.log(decoded);
+        next();
+      });
+    };
+
+    app.get("/api/v1/categories", gateman, async (req, res) => {
       const cursor = CategoriesCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -44,7 +63,25 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("");
+    app.post("/api/v1/auth/access-token", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET);
+      // console.log(token);
+      // res.send(token);
+      res
+        .cookie(
+          "token",
+          token,
+          {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+          },
+          { expiresIn: 60 * 60 }
+        )
+        .send({ success: true });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
